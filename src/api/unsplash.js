b68ada1f4e3b1f0b0e58590906a9da460f3735d4
Anand Shah <anandshah258@gmail.com>
2021-04-22 20:31:27 +0530

    deploying

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: process.env.REACT_APP_API_KEY
+    Authorization: 'Client-ID WGyG9-_Wxmn-d6BHgs8i_gVspj4SCiVHRuPmxoZEYz8'
   }
 })
